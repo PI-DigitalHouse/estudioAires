bbd3139425f39ceb87e82939bb9f0e56cd259684
Camila Queiroz <camimq85@gmail.com>
2021-05-15 16:57:35 -0300

    Regulariza rota para novo orçamento (orcamentoSemLogin.ejs), bem como acesso ao CSS da página.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/orcamento', require('./routes/orcamento'));
+app.use('/novo', require('./routes/orcamentoSemLogin'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/orcamentoSemLogin.js
+++ routes/orcamentoSemLogin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/novo', function(req, res, next) {
+router.get('/', function(req, res, next) { //acesso no localhost3000/novo
     res.render('orcamentoSemLogin');
   });
 
