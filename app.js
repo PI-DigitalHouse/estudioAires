37fbffe1013120cbddfb499dd38878e88bfdd46f
Camila Queiroz <camimq85@gmail.com>
2021-05-15 15:42:47 -0300

    Adiciona partial header nas páginas orcamento.ejs e orcamentoSemLogin.ejs.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/orcamento', require('./routes/orcamento'));
 
