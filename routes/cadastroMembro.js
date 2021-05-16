3e2a3e7625eb4e0873cea09b582da9b9d425df55
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-05-16 11:20:14 -0300

    Adciona rota de envio de infos do cadastro

--- routes/cadastroMembro.js
+++ routes/cadastroMembro.js
@@ -6,4 +6,9 @@ router.get('/membro', function(req, res, next) {
   res.render('cadastroMembro', { title: 'cadastroMembro' });
 });
 
+router.post('/membro', function(req, res, next) {
+  console.log(req.body)
+  res.send(201);
+});
+
 module.exports = router;
