d4084736127946477ccd6d7b8d24ebc4020556f8
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-05-17 11:14:11 -0300

    cria rota de cadastro usuário

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyparser = require('body-parser')
 
 const indexRouter = require('./routes/index');
 const dashboardUsuarioRouter = require ('./routes/dashboardUsuario')
@@ -18,14 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyparser())
 
 app.use('/', indexRouter);
 app.use('/orcamento', require('./routes/orcamento'));
 app.use('/novo', require('./routes/orcamentoSemLogin'));
 app.use('/cadastro', require('./routes/cadastroMembro'));
 app.use('/dashboardUsuario', dashboardUsuarioRouter);
-
+app.use('/cadastroUsuario', require('./routes/cadastro'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
