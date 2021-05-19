38295465c1bb2a8c3d74ae941b0880391cccca35
Amanda Cardoso <amanda.cardoso@sophos.com>
2021-05-19 01:01:50 -0300

    add encriptacao e json usuarios cadastrados e add bcryptlib

--- routes/cadastro-usuario.js
+++ routes/cadastro-usuario.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const fs =require ('fs'); //lib para manipular arquivo do sistema operacional
+const bcrypt =require('bcrypt'); //chamando a lib para crip de hash de cadastro
+
+
+const usuariosCadastrados=[];
 
 /* GET cadastroUsuario */
  //localhost:3000/cadastro-usuario
@@ -7,4 +12,35 @@ router.get('/', function(req, res, next) {
 
 res.render('cadastro-usuario');
 });
+
+router.post('/novo', function(req, res, next) { //rota para criacao do cadastro localhost/cadastro/novo
+    const dadosDoFormulario = req.body
+    dadosDoFormulario.psw =hash(dadosDoFormulario.psw) //encriptando a senha
+    dadosDoFormulario.psw2 =hash(dadosDoFormulario.psw2) 
+    usuariosCadastrados.push(dadosDoFormulario) //salvando oss dados
+    salvarUsuario(usuariosCadastrados)  //chamar a funcao para salvar o usuario no JSON
+    res.redirect('/') //redireciiona para home
+  
+  
+    });
+    
+  
+    function salvarUsuario (usuario){
+      const str = JSON.stringify(usuario) //transforma usuario em string
+      fs.writeFileSync('usuariosCadastrados.json', str)// criando o json com os usuarios cadastrados na string
+      
+    }
+    
+    function hash(obj){
+      
+      const salt =bcrypt.genSaltSync(10)
+      const psw = bcrypt.hashSync(obj, salt)
+      return psw; 
+  
+    }
+   
+  
+
+  
+
 module.exports = router;
