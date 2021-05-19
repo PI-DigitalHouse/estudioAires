494d4e0c0b3630906ae1582c924175f23056c39e
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-05-19 15:04:37 -0300

    cria sistema de autenticação de usuário (problema com identificação de senha

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usuarios = require('../usuariosCadastrados.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
+
+router.get('/login',function(req, res, next){
+  res.render('login')
+})
+
+router.post('/login/autentica',function(req, res, next){
+  
+  for (var i = 0; i < usuarios.length; i++){
+    if (req.body.email === usuarios[i].email && req.body.senha === usuarios[i].senha){
+      res.render ('/dashboardMeuPerfil')
+    }
+  }
+  res.send('Usuário ou senha inválidos')
+})
 
 module.exports = router;
