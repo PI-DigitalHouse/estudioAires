8556e15f0b3167977c4d8d04b0e376dd588f0b18
Mariana Brodersen <mpbrodersen@gmail.com>
2021-05-23 10:57:57 -0300

    ajusta links servicos home e organiza pastas views

--- controllers/servicosController.js
+++ controllers/servicosController.js
@@ -1,31 +1,31 @@
 module.exports.listServico1 = (req, res) => {
-    res.render('servico1', { 
+    res.render('servicos/servico1', { 
       title: 'Servico 1'});
 }
 
 module.exports.listServico2 = (req, res) => {
-    res.render('servico2', { 
+    res.render('servicos/servico2', { 
       title: 'Servico 2'});
 }
 
 module.exports.listServico3 = (req, res) => {
-    res.render('servico3', { 
+    res.render('servicos/servico3', { 
       title: 'Servico 3'});
 }
 
 module.exports.listServico4 = (req, res) => {
-    res.render('servico4', { 
+    res.render('servicos/servico4', { 
       title: 'Servico 4'});
 }
 
 module.exports.listServico5 = (req, res) => {
-    res.render('servico5', { 
+    res.render('servicos/servico5', { 
       title: 'Servico 5'});
 }
 
 
 module.exports.listServico6 = (req, res) => {
-    res.render('servico6', { 
+    res.render('servicos/servico6', { 
       title: 'Servico 6'});
 }
 
