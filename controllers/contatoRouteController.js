4c10e2ae4e27bf3c7dfcf067405c93ae3e846ac1
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-07-01 10:49:54 -0300

    finaliza controller contatoRoute

--- controllers/contatoRouteController.js
+++ controllers/contatoRouteController.js
@@ -1,3 +1,23 @@
+const fs = require('fs')
+
+const readFile = () => {
+    const content = fs.readFileSync('./mensagemDeContato.json', 'utf-8')
+    return JSON.parse(content)
+}
+
+const writeFile = (content) => {
+    const updateFile = JSON.stringify(content)
+    fs.writeFileSync('./mensagemDeContato.json', updateFile, 'utf-8')
+}
+
 module.exports.renderiza = (req, res) => {
     res.render('contato');
+}
+
+module.exports.postContato = (req, res) => {
+    const dados = { nome: req.body.nome, email: req.body.email, mensagem: req.body.mensagem }
+    const currentContent = readFile()
+    currentContent.push({ dados })
+    writeFile(currentContent)
+    res.redirect('/')
 }
