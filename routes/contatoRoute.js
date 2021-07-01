5aa8c1e81e3966bb4523bfd5720f5f504ada88be
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-07-01 11:01:56 -0300

    separa controller da rota da home

--- routes/contatoRoute.js
+++ routes/contatoRoute.js
@@ -1,7 +1,7 @@
-const { Router } = require('express');
+
 const express = require('express');
 const router = express.Router();
-const fs = require('fs')
+
 const {renderiza, postContato} = require('../controllers/contatoRouteController')
 
 
--- routes/home.js
+++ routes/home.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const usuarios = require('../usuariosCadastrados.json')
 const bcrypt = require('bcrypt')
 const fs = require('fs')
+const{getHome,recuSenha, getLogin, autenticacao} = require('../controllers/homeController');
+
 
 // gets da home
-router.get('/', function(req, res) {
-    res.render('home');
-});
+router.get('/', getHome)
 
 //↓↓↓↓↓↓ PARTE DE RECUPERAR SENHA AQUI ATÉ TIRAR DÚVIDA com require de fs lá em cima pra pegar
 
@@ -21,10 +21,7 @@ router.get('/', function(req, res) {
 //   fs.writeFileSync('./emailDeRecuperacao.json', updateFile, 'utf-8')
 // }
 
-router.get('/recuperarSenha', (req, res) => {
-    res.render('DU_recuperacaoSenha')
-})
-
+router.get('/recuperarSenha', recuSenha);
 // router.post('/receberEmail', (req, res) => {
 //   const { email } = req.body
 //   const currentContent = readFile()
@@ -36,20 +33,8 @@ router.get('/recuperarSenha', (req, res) => {
 //↑↑↑↑↑↑ PARTE DE RECUPERAR SENHA AQUI ATÉ TIRAR DÚVIDA com require de fs lá em cima pra pegar
 
 //COISAS PARA COLOCAR AINDA (DEIXAR ORGANIZADO)
-router.get('/login', function(req, res) {
-    res.render('login')
-})
+router.get('/login', getLogin);
 
-router.post('/login/autentica', function(req, res) {
-
-    for (var i = 0; i < usuarios.length; i++) {
-
-        bcrypt.compare(usuarios[i].senha, req.body.senha)
-        if (req.body.email === usuarios[i].email && req.body.senha === usuarios[i].senha) {
-            res.render('/dashboardMeuPerfil')
-        }
-    }
-    res.send('Usuário ou senha inválidos')
-})
+router.post('/login/autentica', autenticacao);
 
 module.exports = router;
