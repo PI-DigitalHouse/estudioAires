830c63f8024a4cc971eda80c9da1440b4d2ef71e
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-07-01 11:13:27 -0300

    separa controller da rota da página de orçamento

--- routes/orcamento.js
+++ routes/orcamento.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-const {renderizaOrcamento, } = require ('../controllers/orcamentoController')
+const {renderizaOrcamento, novoOrcamento } = require ('../controllers/orcamentoController')
 
 
 
