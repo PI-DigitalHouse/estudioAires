fa2711938249fc5cbcd8ce1c839479147929d60e
Matpierro <matpierro570@gmail.com>
2021-07-11 16:27:08 -0300

    Criei rota para a area de alteração de dados e pagina de alteração de dados improvisada para criar o update no banco

--- controllers/DUController.js
+++ controllers/DUController.js
@@ -1,25 +1,25 @@
-
 // dados para teste
 const array = [{
-    idServico: 3,
-    nomeArquivo: 'salaDeEstar_05042021.zip',
-    tamanho: '5 MB'
-},
-{
-    idServico: 3,
-    nomeArquivo: 'salaDeJantar_05042021.zip',
-    tamanho: '25 MB'
-},
-{
-    idServico: 3,
-    nomeArquivo: 'quarto_05042021.zip',
-    tamanho: '250 MB'
-},
-{
-    idServico: 3,
-    nomeArquivo: 'cozinha_05042021.zip',
-    tamanho: '25 MB'
-}];
+        idServico: 3,
+        nomeArquivo: 'salaDeEstar_05042021.zip',
+        tamanho: '5 MB'
+    },
+    {
+        idServico: 3,
+        nomeArquivo: 'salaDeJantar_05042021.zip',
+        tamanho: '25 MB'
+    },
+    {
+        idServico: 3,
+        nomeArquivo: 'quarto_05042021.zip',
+        tamanho: '250 MB'
+    },
+    {
+        idServico: 3,
+        nomeArquivo: 'cozinha_05042021.zip',
+        tamanho: '25 MB'
+    }
+];
 
 const array2 = [{
     idUsuario: 3,
@@ -30,7 +30,7 @@ const array2 = [{
     dataServico: '5 de março de 2020',
     servicosContratados: ['Fotografia', ' Fotografia 3D', ' Vídeo'],
     statusServico: 'Sessão agendada'
-},{
+}, {
     idUsuario: 3,
     idServico: 4,
     endereco: 'Rua X',
@@ -39,7 +39,7 @@ const array2 = [{
     dataServico: '5 de março de 2020',
     servicosContratados: ['Fotografia', ' Fotografia 3D', ' Vídeo'],
     statusServico: 'Sessão em andamento'
-},{
+}, {
     idUsuario: 3,
     idServico: 5,
     endereco: 'Rua X',
@@ -48,7 +48,7 @@ const array2 = [{
     dataServico: '5 de março de 2020',
     servicosContratados: ['Fotografia', ' Fotografia 3D', ' Vídeo'],
     statusServico: 'Imagens em edição'
-},{
+}, {
     idUsuario: 3,
     idServico: 6,
     endereco: 'Rua X',
@@ -57,7 +57,7 @@ const array2 = [{
     dataServico: '5 de março de 2020',
     servicosContratados: ['Fotografia', ' Fotografia 3D', ' Vídeo'],
     statusServico: 'Imagens postadas'
-},{
+}, {
     idUsuario: 3,
     idServico: 7,
     endereco: 'Rua X',
@@ -66,42 +66,51 @@ const array2 = [{
     dataServico: '5 de março de 2020',
     servicosContratados: ['Fotografia', ' Fotografia 3D', ' Vídeo'],
     statusServico: 'Serviço cancelado'
-}
-];
+}];
 
 module.exports.listEntregaveis = (req, res) => {
-    res.render('dashboardUsuario_entregaveis', { 
-      title: 'Entregáveis', 
-      entregaveis: array,
-      dadosUsuario: req.session.usuario });
+    res.render('dashboardUsuario_entregaveis', {
+        title: 'Entregáveis',
+        entregaveis: array,
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.listSolicitacoes = (req, res) => {
-    res.render('dashboardUsuario_solicitacoes', { 
-      title: 'Solicitações', 
-      solicitacoes: array2 ,
-      dadosUsuario: req.session.usuario});
+    res.render('dashboardUsuario_solicitacoes', {
+        title: 'Solicitações',
+        solicitacoes: array2,
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.recuperaSenha = (req, res) => {
-    res.render('DU_recuperacaoSenha', { 
-      title: 'Recuperação de Senha',
-      dadosUsuario: req.session.usuario});
+    res.render('DU_recuperacaoSenha', {
+        title: 'Recuperação de Senha',
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.alteraSenha = (req, res) => {
-    res.render('dashboardUsuario_alteracaoSenha',{
+    res.render('dashboardUsuario_alteracaoSenha', {
         title: 'Altere a sua Senha',
-        dadosUsuario: req.session.usuario});
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.meuPerfil = (req, res) => {
     console.log(req)
     res.render('dashboardUsuario_meuPerfil', {
-        title: 'Meu Perfil',
-        dadosUsuario: req.session.usuario
-    }
-        
-        );
+            title: 'Meu Perfil',
+            dadosUsuario: req.session.usuario
+        }
+
+    );
 }
 
+module.exports.alteraDados = (req, res) => {
+    res.render('alterarDados', {
+        title: 'Alterar Dados',
+        dadosUsuario: req.session.usuario
+    })
+}
