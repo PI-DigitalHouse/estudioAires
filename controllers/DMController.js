0ed60450e019ef4156cddb93f63d37c31040ed9d
Mariana Brodersen <mpbrodersen@gmail.com>
2021-07-11 16:39:44 -0300

    ajusta formato datas

--- controllers/DMController.js
+++ controllers/DMController.js
@@ -1,4 +1,5 @@
 const models = require('../models');
+const session = require('express-session');
 
 module.exports.calendario = (req, res) => {
     res.render('DMCalendario', {
@@ -9,9 +10,11 @@ module.exports.calendario = (req, res) => {
 module.exports.bloquear = (async (req, res, next) => {
   const bloqueio = req.body
 
-  bloqueio.reservadoPor = req.session.nome
-  bloqueio.aceitoPor = req.session.nome
+  bloqueio.reservadoPor = req.session.usuario.idUsuario
+  bloqueio.aceitoPor = req.session.usuario.idUsuario
   bloqueio.idSolicitacao = 1
+ 
+  console.log(bloqueio)
 
   await models.Reserva.create (bloqueio)
 
