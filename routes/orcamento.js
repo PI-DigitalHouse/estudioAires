38e9942157fe3f4ebaa2541ed65abc985af3f71c
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-07-12 13:07:34 -0300

    corrigi erro da página de orçamento logado

--- routes/orcamento.js
+++ routes/orcamento.js
@@ -2,18 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
-const {renderizaOrcamento, novoOrcamento, renderizaOrcamentoSLogin } = require ('../controllers/orcamentoController')
-
+const {renderizaOrcamento, novoOrcamento,  } = require ('../controllers/orcamentoController')
 const checkSession = require('../middlewares/checkSession') 
 
 
 /* GET users listing. */
 router.get('/', checkSession, renderizaOrcamento);
-router.get('/novo', renderizaOrcamentoSLogin);
 
 //ajax ou fetin 
-
-router.post('/novoOrcamento', renderizaOrcamento);
+router.post('/novoOrcamento', novoOrcamento);
 
 
 
--- routes/orcamentoSemLogin.js
+++ routes/orcamentoSemLogin.js
@@ -5,10 +5,10 @@ const router = express.Router();
 /*pode apagar const {renderizaOrcamento, novoOrcamento } = require ('../controllers/orcamentoController')
 const checkSession = require('../middlewares/checkSession') 
 const renderizaOrcamentoSLogin = require ('../controllers/orcamentoSemLoginController')
+  */
 
 
-
-/* GET users listing. 
+ //GET users listing. 
 router.get('/', renderizaOrcamentoSLogin);
 
-module.exports = router;  */
+module.exports = router;  
