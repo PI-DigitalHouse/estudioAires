cb571d11e7fa9378e946c0a3095718ae56a9823b
henriquepbalsimelli <henriquepbalsimelli@outlook.com>
2021-07-12 12:00:43 -0300

    adiciona função de calcular orçamento

--- controllers/orcamentoController.js
+++ controllers/orcamentoController.js
@@ -2,6 +2,8 @@ const bcrypt =require('bcrypt'); //chamando a lib para crip de hash de cadastro
 const orcamentosCadastrados=[];
 const session = require('express-session');
 const fs =require ('fs'); //lib para manipular arquivo do sistema operacional
+
+
 module.exports.renderizaOrcamento = (req,res,next) => {
     res.render('orcamento', {
         title : 'Novo Orçamento',
@@ -9,16 +11,39 @@ module.exports.renderizaOrcamento = (req,res,next) => {
 
 })};
 
-module.exports.novoOrcamento = (req,res,next) => {
-    
-  const dadosDoFormulario = req.body
-  orcamentosCadastrados.push(dadosDoFormulario) //adicionando os dadosdo formulario no array
-  salvarUsuario(orcamentosCadastrados)  //chamar a funcao para salvar o orcamento no JSON
-  res.redirect('/') //redireciiona para home
 
+
+module.exports.novoOrcamento = (req,res,next) => {
+  
+  const medidasImovel = parseInt(req.body.MedidaImovel)
+  const fotografiaS = parseFloat(req.body.Fotografia)
+  const fotografia3603d = parseFloat(req.body.Fotografia3603d)
+  const videoDinamico = parseFloat(req.body.VideoDinamico)
+  const imagensAereas = parseFloat(req.body.ImagensAereas)
+
+  const dadosDoFormulario = req.body
+  const orcamento = calculaOrcamento(medidasImovel, fotografiaS, fotografia3603d, videoDinamico, imagensAereas)
+
+  orcamentosCadastrados.push(dadosDoFormulario) //adicionando os dadosdo formulario no array
+  orcamentosCadastrados.push(orcamento)
+  
+  console.log(orcamentosCadastrados)
+  console.log(medidasImovel)
+  
+  //salvarUsuario(orcamentosCadastrados)  //chamar a funcao para salvar o orcamento no JSON
+  //res.redirect('/') //redireciiona para home
+ 
 }
 function salvarUsuario (usuario){
     const str = JSON.stringify(usuario) //transforma usuario em string
     fs.writeFileSync('orcamentosCadastrados.json', str)// criando o json com os usuarios cadastrados na string
     
   }
+
+ function calculaOrcamento(medida,serv1, serv2, serv3, serv4 ){
+    
+    const valorTotal = medida*(0.8 + serv1 + serv2 + serv3 + serv4 )
+    
+    return valorTotal
+ }
+
