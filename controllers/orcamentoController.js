f2fdbf963dbe25fe2380da164ccce06a12d14dbb
Mariana Brodersen <mpbrodersen@gmail.com>
2021-07-23 19:32:20 -0300

    ajusta dtp orçamento

--- controllers/orcamentoController.js
+++ controllers/orcamentoController.js
@@ -1,68 +1,79 @@
-const bcrypt =require('bcrypt'); //chamando a lib para crip de hash de cadastro
+const bcrypt = require('bcrypt'); //chamando a lib para crip de hash de cadastro
 const session = require('express-session');
-const fs =require ('fs'); //lib para manipular arquivo do sistema operacional
+const fs = require('fs'); //lib para manipular arquivo do sistema operacional
 const models = require('../models');
 const { Op } = require('sequelize');
 const servico = require('../models/servico');
 
-let orcamentosCadastrados=[];
+let orcamentosCadastrados = [];
 
 
-module.exports.renderizaOrcamento = (req,res,next) => {
+module.exports.renderizaOrcamento = (req, res, next) => {
 
-if(req.session.usuario){
+const horariosBloqueados = async () => await models.Reserva.findAll({
+  where: {
+    idReserva: {
+      [Op.like]: `${idReserva || ''}%`,
+    }
+  }
+})
+
+  if (req.session.usuario) {
+
+    res.render('orcamento', {
+      title: 'Novo Orçamento',
+      dadosUsuario: req.session.usuario,
+      horariosBloqueados: horariosBloqueados
+    })
+  } else {
+    console.log('n logado')
+    res.render('orcamentoSLogin', {
+      title: 'Novo Orçamento',
+      dadosUsuario: req.session.usuario,
+      horariosBloqueados: horariosBloqueados
+    })
+  }
+}
+
+
+
+module.exports.novoOrcamento = (async (req, res, next) => {
   
-  res.render('orcamento', {
-    title : 'Novo Orçamento',
-    dadosUsuario: req.session.usuario
-  })
-}else{
-  console.log('n logado')
-  res.render('orcamentoSLogin', {
-    title : 'Novo Orçamento',
-    dadosUsuario: req.session.usuario
-  })
-}
-}
-
-
-
-module.exports.novoOrcamento = (async (req,res,next) => {
 
   const dadosDoFormulario = req.body
 
 
-  const {valor, idUsuario}=req.query
-  
+  const { valor, idUsuario } = req.query
+
 
 
   const newService = await models.Servico.findOne({
     valor: {
       [Op.like]: `${valor || ''}%`,
     },
-    
+
   })
-  const valorFront = calValorfront =()=>{
-    dadosDoFormulario.tamanhoImovel*(0.2+juncao.length)
+  const valorFront = calValorfront = () => {
+    dadosDoFormulario.tamanhoImovel * (0.2 + juncao.length)
   }
 
 
   req.body.horarioFinal = req.body.horarioInicio
   let juncao = orcamentosCadastrados.concat(dadosDoFormulario.servico)
 
-console.log(newService.valor)
-console.log(dadosDoFormulario.servico)
-console.log(juncao.length) //percorrendo o array e trazendo a quantidade de servico que tem nele
+  console.log(newService.valor)
+  console.log(dadosDoFormulario.servico)
+  console.log(juncao.length) //percorrendo o array e trazendo a quantidade de servico que tem nele
 
 
-  const resultado = calculaOrcamento(dadosDoFormulario.tamanhoImovel, newService.valor, juncao.length )
+  const resultado = calculaOrcamento(dadosDoFormulario.tamanhoImovel, newService.valor, juncao.length)
   //req.body.valor = resultado
 
 
- dadosDoFormulario.valor = resultado
-dadosDoFormulario.reservadoPor = req.session.usuario.idUsuario
- dadosDoFormulario.membros_idMembro = 1
- dadosDoFormulario.status ='active'
+  dadosDoFormulario.valor = resultado
+  dadosDoFormulario.reservadoPor = req.session.usuario.idUsuario
+  dadosDoFormulario.membros_idMembro = 1
+  dadosDoFormulario.status = 'active'
 
   const reservas = await models.Reserva.create(dadosDoFormulario)
 
@@ -70,21 +81,21 @@ dadosDoFormulario.reservadoPor = req.session.usuario.idUsuario
     res.render('orcamento')
     return
   }
-  
-  dadosDoFormulario.reservas_idReserva=reservas.idReserva
-  
+
+  dadosDoFormulario.reservas_idReserva = reservas.idReserva
+
   await models.Orcamento.create(dadosDoFormulario)
 
   res.redirect('/') //redireciiona para home
- 
+
 
 })
 
 
 
- function calculaOrcamento(tamanhoImovel, servico, valor ){
-    
-let valorTotal = tamanhoImovel*(valor * servico)
-    return valorTotal
- }
+function calculaOrcamento(tamanhoImovel, servico, valor) {
+
+  let valorTotal = tamanhoImovel * (valor * servico)
+  return valorTotal
+}
 
