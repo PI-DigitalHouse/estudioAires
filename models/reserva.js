d5f41b68969f6c874506e9e54f5f3119aa121891
Camila Queiroz <camimq85@gmail.com>
2021-07-23 17:40:07 -0300

    Trabalho no DMController para exibir jobs finalizados.

--- models/reserva.js
+++ models/reserva.js
@@ -1,58 +1,56 @@
 'use strict';
-const {
-    Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class Reserva extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // Associação com Usuario
-            this.belongsTo(models.Usuario, {
-                    foreignKey: 'reservadoPor',
-                    id: 'idUsuario',
-                    as: 'usuarios'
-                }),
-
-                // Associação com Usuario
-                this.belongsTo(models.Usuario, {
-                    foreignKey: 'aceitoPor',
-                    id: 'idUsuario',
-                   
-                })
-              
-        }
-    };
-    Reserva.init({
-        idReserva: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
+  class Reserva extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Associação com Usuario
+      this.belongsTo(models.Usuario, {
+        foreignKey: 'reservadoPor',
+        id: 'idUsuario',
+        as: 'usuarios',
+      }),
+        // Associação com Usuario
+        this.belongsTo(models.Usuario, {
+          foreignKey: 'aceitoPor',
+          id: 'idUsuario',
+        });
+    }
+  }
+  Reserva.init(
+    {
+      idReserva: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      reservadoPor: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Usuarios',
         },
-        reservadoPor: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Usuarios'
-            },
-            allowNull: false
+        allowNull: false,
+      },
+      aceitoPor: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Usuarios',
         },
-        aceitoPor: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Usuarios'
-            },
-            allowNull: false
-        },
-        horarioInicio: DataTypes.DATE,
-        horarioFinal: DataTypes.DATE,
-        membros_idMembro:  DataTypes.INTEGER,
-        status: DataTypes.STRING(150)
-    }, {
-        sequelize,
-        modelName: 'Reserva',
-    });
-    return Reserva;
+        allowNull: false,
+      },
+      horarioInicio: DataTypes.DATE,
+      horarioFinal: DataTypes.DATE,
+      membros_idMembro: DataTypes.INTEGER,
+      status: DataTypes.STRING(150),
+    },
+    {
+      sequelize,
+      modelName: 'Reserva',
+    },
+  );
+  return Reserva;
 };
