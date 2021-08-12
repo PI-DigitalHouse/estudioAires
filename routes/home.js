1ec155a03460d8d6513583672780d2a6f6114e34
Matpierro <matpierro570@gmail.com>
2021-08-12 12:00:34 -0300

    Consertando funcionalidade de login

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,9 @@ const models = require('../models');
 const session = require('express-session');
 const router = express.Router();
 const bcrypt = require('bcrypt')
-const { getHome, recuSenha, getLogin, logar, logOut, getProjeto1, getProjeto2, getProjeto3 } = require('../controllers/homeController');
+const { getHome, recuSenha, logOut, getProjeto1, getProjeto2, getProjeto3, logar } = require('../controllers/homeController');
 const logarMembro = require('../controllers/autenticacaoMembro')
 const checkSession = require('../middlewares/checkSession')
-
 
 router.get('/', getHome)
 
@@ -20,15 +19,8 @@ router.get('/projetos/projeto3', getProjeto3)
 
 router.get('/recuperarSenha', recuSenha);
 
-router.get('/login', getLogin);
-
-
 router.post('/login', logar);
 
-
 router.get('/logout', logOut);
-
-
-
 
 module.exports = router;
