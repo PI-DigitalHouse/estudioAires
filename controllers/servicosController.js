46004c997d9be8bc8506926fc5f9bd4164a4e4e8
Matpierro <matpierro570@gmail.com>
2021-08-12 15:45:24 -0300

    Correção de rotas serviços(Melhor deixar tudo no arquivo da home já que é conteúdo só da home)

--- controllers/servicosController.js
+++ controllers/servicosController.js
@@ -1,37 +1,26 @@
 module.exports.listServico1 = (req, res) => {
-    res.render('servicos/servico1', { 
-      title: 'Servico 1'});
+    res.render('/servico1', {
+        title: 'Servico 1'
+    });
 }
 
 module.exports.listServico2 = (req, res) => {
-    res.render('servicos/servico2', { 
-      title: 'Servico 2',
-      dadosUsuario: req.session.usuario});
+    res.render('servico2', {
+        title: 'Servico 2',
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.listServico3 = (req, res) => {
-    res.render('servicos/servico3', { 
-      title: 'Servico 3',
-      dadosUsuario: req.session.usuario});
+    res.render('servico3', {
+        title: 'Servico 3',
+        dadosUsuario: req.session.usuario
+    });
 }
 
 module.exports.listServico4 = (req, res) => {
-    res.render('servicos/servico4', { 
-      title: 'Servico 4',
-      dadosUsuario: req.session.usuario});
+    res.render('servico4', {
+        title: 'Servico 4',
+        dadosUsuario: req.session.usuario
+    });
 }
-
-module.exports.listServico5 = (req, res) => {
-    res.render('servicos/servico5', { 
-      title: 'Servico 5',
-      dadosUsuario: req.session.usuario});
-}
-
-
-module.exports.listServico6 = (req, res) => {
-    res.render('servicos/servico6', { 
-      title: 'Servico 6',
-      dadosUsuario: req.session.usuario});
-}
-
-
--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const models = require('../models');
 const session = require('express-session');
 const router = express.Router();
 const bcrypt = require('bcrypt')
+const { listServico1, listServico2, listServico3, listServico4, listServico5, listServico6 } = require('../controllers/servicosController');
 const { getHome, recuSenha, logOut, getProjeto1, getProjeto2, getProjeto3, logar } = require('../controllers/homeController');
 const logarMembro = require('../controllers/autenticacaoMembro')
 const checkSession = require('../middlewares/checkSession')
@@ -10,17 +11,34 @@ const checkSession = require('../middlewares/checkSession')
 router.get('/', getHome)
 
 router.get('/', checkSession, getHome)
+    /* GET serviço 1 */
+router.get('/servico1', listServico1);
 
+/* GET serviço 2 */
+router.get('/servico2', listServico2);
+
+/* GET serviço 3 */
+router.get('/servico3', listServico3);
+
+/* GET serviço 4 */
+router.get('/servico4', listServico4);
+
+/* GET projeto 1 */
 router.get('/projetos/projeto1', getProjeto1)
 
+/* GET projeto 2 */
 router.get('/projetos/projeto2', getProjeto2)
 
+/* GET projeto 3 */
 router.get('/projetos/projeto3', getProjeto3)
 
+/* GET para recuperação de senha */
 router.get('/recuperarSenha', recuSenha);
 
+/* POST do login */
 router.post('/login', logar);
 
+/* GET Log-out */
 router.get('/logout', logOut);
 
 module.exports = router;
