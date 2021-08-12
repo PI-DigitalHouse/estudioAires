e058378e22b1d71ca3fa1601f1d68d2ce50e971c
Matpierro <matpierro570@gmail.com>
2021-08-12 20:37:12 -0300

    Corrigindo serviços

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -43,6 +43,33 @@ module.exports.getProjeto3 = (req, res) => {
     })
 }
 
+module.exports.listServico1 = (req, res) => {
+    res.render('servico1', {
+        title: 'Servico 1'
+    });
+}
+
+module.exports.listServico2 = (req, res) => {
+    res.render('servico2', {
+        title: 'Servico 2',
+        dadosUsuario: req.session.usuario
+    });
+}
+
+module.exports.listServico3 = (req, res) => {
+    res.render('servico3', {
+        title: 'Servico 3',
+        dadosUsuario: req.session.usuario
+    });
+}
+
+module.exports.listServico4 = (req, res) => {
+    res.render('servico4', {
+        title: 'Servico 4',
+        dadosUsuario: req.session.usuario
+    });
+}
+
 module.exports.recuSenha = (req, res) => {
     res.render('DU_recuperacaoSenha', {
         title: 'Recuperar Senha',
