cd0e50bc915968be9d928f53e3d664bdc76a93c3
cardoso24 <amcardoso@frba.utn.edu.ar>
2021-08-14 11:57:32 -0300

    add sweet alert

--- public/js/sweetAlert.js
+++ public/js/sweetAlert.js
@@ -1,21 +1,41 @@
+
 document.getElementById("btnSweetAlert").addEventListener("click", abreAlertDelete)
 function abreAlertDelete(){
-
-Swal.fire({
-    title: 'Tem certeza que deseja cancelar esse serviço?',
-    text: "Ao cancelar nao sera possivel reativar essa solicitacao",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Sim, cancelar'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Cancelado',
-        'Sua solicitacao foi cancelada',
-        'success'
-      )
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Tem certeza que deseja cancelar esse serviço?',
+        text: "Ao cancelar nao sera possivel reativar essa solicitacao",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sim, cancelar!',
+        cancelButtonText: 'Nao cancelar!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Solicitacao cancelada!',
+            'Seu servico foi cancelado com sucesso',
+            'success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Ufa, foi por pouco...',
+            'Sua solicitacao segue ativa:)',
+            'error'
+          )
+        }
+      })
     }
-  })
-}
+
+
+
