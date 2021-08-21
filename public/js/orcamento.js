a3b7d41878d2a147e7e2ec7d98baab3c308b21d4
Camila Queiroz <camimq85@gmail.com>
2021-08-21 18:05:16 -0300

    Identa código.

--- public/js/orcamento.js
+++ public/js/orcamento.js
@@ -101,10 +101,10 @@ function fixStepIndicator2(n) {
   // Esta função remove a classe "ativa" de todos os steps ...
   var i, x = document.getElementsByClassName("step-selection");
   for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
+    x[i].className = x[i].className.replace("active", "");
   }
   //...  e adiciona a classe "ativa" ao step atual:
-  x[n].className += " active";
+  x[n].className += "active";
 }
 /*border*/
 function fixStepIndicator2(n) {
